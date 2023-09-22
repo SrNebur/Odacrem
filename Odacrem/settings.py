4427755f21504d96d08ecda70adf567484073897
Nebur <rubenmasalmira@gmail.com>
2023-09-22 11:41:25 +0200

    Creación de la app tienda y cambios en los test

--- Odacrem/settings.py
+++ Odacrem/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+#Indicamos a Django que las imagenes se guardan en esta carpeta
+MEDIA_URL='/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
