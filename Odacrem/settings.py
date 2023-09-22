1a2bc9ff3a88a18fcb3fdc486e20ea4f40b0a78a
Nebur <rubenmasalmira@gmail.com>
2023-09-22 11:35:16 +0200

    Conexión a la base de datos y combios en test home

--- Odacrem/settings.py
+++ Odacrem/settings.py
@@ -73,11 +73,16 @@ WSGI_APPLICATION = 'Odacrem.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# Conexión a PostgreSQL
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'odacremDB',
+        'USER':'odacrem',
+        'PASSWORD':'123456', #Contraseña local para la base de datos
+        'HOST':'127.0.0.1',
+        'DATABASE_PORT':'5432',
     }
 }
 
