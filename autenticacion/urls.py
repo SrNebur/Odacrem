6be4bbba67451632b1cebf91528865af50982491
SrNebur <rubenmasalmira@gmail.com>
2023-12-01 17:01:01 +0100

    Modificación en iconos creación de cerrarSesion

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.VRegistro.as_view(),name="Registro"),
     path('login',views.logear, name="Login"),
+    path('cerrar_sesion',cerrar_sesion,name="cerrar_sesion"),
 ]
