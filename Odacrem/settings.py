2b39473ead4caaecaea7cd087df4b5d3335270e7
Nebur <rubenmasalmira@gmail.com>
2023-12-14 01:39:57 +0100

    Cambios en colores admin django

--- Odacrem/settings.py
+++ Odacrem/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "admin_interface",
+    "colorfield",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,6 +49,8 @@ INSTALLED_APPS = [
     "contacto",
     "about",
 ]
+
+X_FRAME_OPTIONS = "SAMEORIGIN"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
